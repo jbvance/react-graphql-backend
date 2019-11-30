d90ad7c2229ba30c1edae2574c54f0f667079852
Jason Vance <jbvance@gmail.com>
2019-11-30 14:14:06 -0600

    end 14

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,8 +1,13 @@
+const { forwardTo } = require('prisma-binding');
+
 const Query = {   
-    dogs(parent, args, ctx, info) {
-        global.dogs = global.dogs || [];
-        return global.dogs || [];
-    }
+items: forwardTo('db')
+// same as above - you can do when the Yoga Query and Prisma Query are the same
+// and you don't care about running any custom logic
+//    async items(parent, args, ctx, info) {
+//        const items = await ctx.db.query.items();
+//        return items;
+//    }
 };
 
 module.exports = Query;
