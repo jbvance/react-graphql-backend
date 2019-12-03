07761040e405d416db22b1adf4316919b1b0fa60
jbvance <jbvance@gmail.com>
2019-12-03 11:22:41 -0500

    update data model, schema, and Mutation for creating user

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -24,11 +27,31 @@ const Mutations = {
     }, info)
 
   },
+  
   async deleteItem(parent, args, ctx, info) {
     const where = { id: args.id };
     const item = await ctx.db.query.item({ where }, `{ id title }`);
     // TODO - verify user owns this item for deletion
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    // hash the password
+    const password = await bcrypt.hash(args.password, 10);
+    const user = await ctx.db.mutation.createUser({
+      data: {
+        ...args,
+        password,
+        permissions: { set: ['USER']}
+      }
+    }, info);
+    //create jwt for user
+    const token = jwt.sign({ userId: user.id}, process.env.APP_SECRET);
+    //set jwt as cookie on response
+    ctx.response.cookie('token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 365 }); // 1 year cookieParser
+    //return user to browser
+    return user;
   }
 };
 
